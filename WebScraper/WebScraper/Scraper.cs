0b45441f755820524f8518d3a1e91517cf482344
Alin Ungureanu <alin.ungureanu1407@gmail.com>
2020-09-18 11:42:37 +0300

    initial web page setup commit

---
+++ WebScraper/WebScraper/Scraper.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    public class Scraper
+    {
+        private String url;
+        public Scraper(String url)
+        {
+            this.url = url;//https://www.pure.co.uk/menus/breakfast/
+        }
+
+        public async void startScraping()
+        {
+            IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+
+            Thread.Sleep(3000);
+
+            var submenu = driver.FindElement(By.XPath("/html/body/main/section/div[1]/div/div[1]/a/h3"));
+
+            //var submenuTitle = submenu.FindElement(By.TagName("h2"));
+
+            Console.WriteLine("First categ is " + submenu.Text);
+
+            driver.Close();
+        }
+    }
+}
